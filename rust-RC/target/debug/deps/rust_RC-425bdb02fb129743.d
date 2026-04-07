de9657a6a875d4d4652e1f5fa893c4b7906a09d4
Roald Van Glabeek <roald@vanglabbeek.us>
2026-04-07 15:12:34 +0200

    added rust target dir, oops

--- rust-RC/target/debug/deps/rust_RC-425bdb02fb129743.d
+++
@@ -1,5 +0,0 @@
-/home/roald/tech_projects/transistor-clock/rust-RC/target/debug/deps/rust_RC-425bdb02fb129743: src/main.rs
-
-/home/roald/tech_projects/transistor-clock/rust-RC/target/debug/deps/rust_RC-425bdb02fb129743.d: src/main.rs
-
-src/main.rs:
--- rust-RC/target/debug/rust-RC.d
+++
@@ -1,1 +0,0 @@
-/home/roald/tech_projects/transistor-clock/rust-RC/target/debug/rust-RC: /home/roald/tech_projects/transistor-clock/rust-RC/src/main.rs
